4ee7c732bcb07bab733f1d487021e435a67a9375
daiti0113 <stennis.ds.0113@gmail.com>
2020-02-11 23:39:15 +0900

    feat:first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <h1>Hello world!</h1>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'));
